f9674c3f11c611233a2e967f68886055292d56c2
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-04-10 23:37:34 +0100

    Refactoring more sass. Removed variables folder for single file. Commented out mixin.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-var skills = ["Front End", "CSS", "JavaScript", "SASS", "BackboneJS", "ES6", "jQuery", "Lodash", "UnderscoreJS", "HandlebarsJS"];
+$(function() {
+
+  var skills = ["Front End", "CSS", "JavaScript", "SASS", "BackboneJS", "ES6", "jQuery", "Lodash", "UnderscoreJS", "HandlebarsJS"];
+
+  console.log('Hello World');
+
+});
