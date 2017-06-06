5872d4d135ecc2f47180e6c9595121477b4be95e
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2017-06-06 23:31:41 +0100

    Fix for skills[i] becoming undefined in loop and erroring in console.

--- js/main.js
+++ js/main.js
@@ -3,13 +3,17 @@ window.onload = function() {
   let zIndex = 0;
 
   function showSkill() {
-    for(let i = 0; i <= skills.length; i++) {
+    for(let i = 0; i <= skills.length -1; i++) {
       setTimeout(function() {
-        if (i >= skills.length) {
+        if (i >= skills.length -1) { // TODO: Fix having to add blank li in html
           showSkill();
         }
         zIndex++;
-        skills[i].style.zIndex = zIndex;
+        if(typeof skills[i] !== 'undefined') {
+          skills[i].style.zIndex = zIndex;
+        } else {
+          debugger;
+        }
       }, 1000 * i);
     }
   }
