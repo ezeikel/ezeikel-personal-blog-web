2943af23bbe9e6a70c09be07fd1090fc50b88b92
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-01-23 22:58:50 +0000

    Improvements to js.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-window.onload = function() {
-  let skills = document.querySelectorAll('.about-me__skills li');
-  let zIndex = 0;
+window.onload = () => {
+  const skills = document.querySelectorAll('.about-me__skills li');
+  let index = 0;
 
-  function showSkill() {
-    for(let i = 0; i <= skills.length -1; i++) {
-      setTimeout(function() {
-        if (i >= skills.length -1) { // TODO: Fix having to add blank li in html
+  const showSkill = () => {
+    [...skills].map((skill,i) => {
+      setTimeout(() => {
+
+        if (i === skills.length -1)  {
           showSkill();
         }
-        zIndex++;
-        if(typeof skills[i] !== 'undefined') {
-          skills[i].style.zIndex = zIndex;
-        } else {
-          debugger;
-        }
-      }, 1000 * i);
-    }
-  }
+
+        index++;
+        skill.style.zIndex = index;
+      }, 1000 * i)
+    });
+  };
 
   showSkill();
+
 }
